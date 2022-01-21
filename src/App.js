7d10fc4e7813c7fdee6e5ccc32bb052860125aaf
kaydayo <olukayodedayo100@gmail.com>
2022-01-21 16:25:15 +0100

    completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import menu from './data'
 import { useState } from 'react'
 import Category from './Category';
 
-
-
+const categories = ["all", ...new Set(menu.map(item => item.category))]
+console.log(categories)
 function App() {
   const [items, setItems] = useState(menu)
+
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setItems(menu)
+      return;
+    }
+    const newItem = menu.filter(item => item.category === category)
+    setItems(newItem)
+
+  }
   return (
     <div className="App">
       <header>
@@ -14,16 +24,27 @@ function App() {
         <div className="underline"></div>
       </header>
       <main>
-        <h2>All</h2>
+        <div className="category-button">
+          {categories.map(category => <Category category={category} filterItems={filterItems} />)}
+        </div>
         <div className="content">
           {items.map(item => {
             return (
-              <div className="item-content">
+              <div className="item-content" key={item.id}>
                 <img src={item.img} alt={item.title} />
-                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
-                  <h5>{item.title}</h5>
-                  <h5 style={{ float: 'right', color: 'orange' }}>${item.price}</h5>
+                <div style={{ display: 'flex', justifyContent: 'space-between', maxWidth: '300px' }}>
+                  <h4>{item.title}</h4>
+                  <h4 style={{ float: 'right', color: 'orange' }}>${item.price}</h4>
                 </div>
+                <hr />
+                <div>
+                  <span className="fa fa-star checked"></span>
+                  <span className="fa fa-star checked"></span>
+                  <span className="fa fa-star checked"></span>
+                  <span className="fa fa-star"></span>
+                  <span className="fa fa-star"></span>
+                </div>
+                <p>{item.desc}</p>
               </div>
             )
           })}
--- src/Category.jsx
+++ src/Category.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const Category = ({category}) => {
-  return <button>{category}</button>;
+const Category = ({ category, filterItems }) => {
+    return <button className="selet-category" onClick={()=>filterItems(category)}>{category}</button>;
 };
 
 export default Category;
